e06ec114f022c0d50fb0649cff8f6a09e5391c25
Ricardo <perezmotaricardo162@gamil.com>
2026-01-08 20:42:46 -0600

    Tarea 3: obtener libros por autor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const booksByAuthor = {};
+  
+  for (let i = 0; i < bookKeys.length; i++) {
+    const isbn = bookKeys[i];
+    if (books[isbn].author === author) {
+      booksByAuthor[isbn] = books[isbn];
+    }
+  }
+  
+  if (Object.keys(booksByAuthor).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(JSON.stringify(booksByAuthor, null, 2));
+  } else {
+    return res.status(404).json({message: "No books found for this author"});
+  }
 });
 
 // Get all books based on title
